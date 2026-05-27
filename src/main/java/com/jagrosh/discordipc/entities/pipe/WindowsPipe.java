3a1434cbb32f02ed7b57e2e2e856635ecb68cca3
yvhvgcdr-alt <yvhvgcdr@gmail.com>
2026-05-28 02:36:51 +0700

    Update WindowsPipe.java

--- src/main/java/com/jagrosh/discordipc/entities/pipe/WindowsPipe.java
+++ src/main/java/com/jagrosh/discordipc/entities/pipe/WindowsPipe.java
@@ -1,89 +1,162 @@
-/*
- * Copyright 2017 John Grosh (john.a.grosh@gmail.com).
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.jagrosh.discordipc.entities.pipe;
 
 import com.jagrosh.discordipc.IPCClient;
 import com.jagrosh.discordipc.entities.Callback;
 import com.jagrosh.discordipc.entities.Packet;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+
 import java.util.HashMap;
 
 public class WindowsPipe extends Pipe
 {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(WindowsPipe.class);
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(WindowsPipe.class);
 
     private final RandomAccessFile file;
 
-    WindowsPipe(IPCClient ipcClient, HashMap<String, Callback> callbacks, String location)
-    {
+    WindowsPipe(
+            IPCClient ipcClient,
+            HashMap<String, Callback> callbacks,
+            String location
+    ) {
         super(ipcClient, callbacks);
-        try {
-            this.file = new RandomAccessFile(location, "rw");
-        } catch (FileNotFoundException e) {
+
+        try
+        {
+            this.file = new RandomAccessFile(
+                    location,
+                    "rw"
+            );
+        }
+        catch(FileNotFoundException e)
+        {
             throw new RuntimeException(e);
         }
     }
 
     @Override
-    public void write(byte[] b) throws IOException {
+    public void write(byte[] b) throws IOException
+    {
+        if(status == PipeStatus.CLOSED)
+        {
+            throw new IOException(
+                    "Pipe is closed!"
+            );
+        }
+
         file.write(b);
+
+        file.getFD().sync();
     }
 
     @Override
-    public Packet read() throws IOException, JSONException {
-        while(file.length() == 0 && status == PipeStatus.CONNECTED)
-        {
-            try {
+    public Packet read() throws IOException, JSONException
+    {
+        while(
+                file.length() == 0
+                        && status == PipeStatus.CONNECTED
+                        && !Thread.currentThread().isInterrupted()
+        ) {
+            try
+            {
                 Thread.sleep(50);
-            } catch(InterruptedException ignored) {}
+            }
+            catch(InterruptedException e)
+            {
+                Thread.currentThread().interrupt();
+
+                throw new IOException(
+                        "IPC read thread interrupted",
+                        e
+                );
+            }
         }
 
-        if(status==PipeStatus.DISCONNECTED)
-            throw new IOException("Disconnected!");
+        if(status == PipeStatus.DISCONNECTED)
+        {
+            throw new IOException(
+                    "Disconnected!"
+            );
+        }
 
-        if(status==PipeStatus.CLOSED)
-            return new Packet(Packet.OpCode.CLOSE, null);
+        if(status == PipeStatus.CLOSED)
+        {
+            return new Packet(
+                    Packet.OpCode.CLOSE,
+                    null
+            );
+        }
 
-        Packet.OpCode op = Packet.OpCode.values()[Integer.reverseBytes(file.readInt())];
-        int len = Integer.reverseBytes(file.readInt());
+        Packet.OpCode op =
+                Packet.OpCode.values()[
+                        Integer.reverseBytes(
+                                file.readInt()
+                        )
+                ];
+
+        int len =
+                Integer.reverseBytes(
+                        file.readInt()
+                );
+
         byte[] d = new byte[len];
 
         file.readFully(d);
-        Packet p = new Packet(op, new JSONObject(new String(d)));
-        LOGGER.debug(String.format("Received packet: %s", p.toString()));
+
+        Packet p = new Packet(
+                op,
+                new JSONObject(new String(d))
+        );
+
+        LOGGER.debug(
+                "Received packet: {}",
+                p
+        );
+
         if(listener != null)
-            listener.onPacketReceived(ipcClient, p);
+        {
+            listener.onPacketReceived(
+                    ipcClient,
+                    p
+            );
+        }
+
         return p;
     }
 
     @Override
-    public void close() throws IOException {
+    protected void closePipe() throws IOException
+    {
         LOGGER.debug("Closing IPC pipe...");
-        send(Packet.OpCode.CLOSE, new JSONObject(), null);
-        status = PipeStatus.CLOSED;
-        file.close();
-    }
 
+        try
+        {
+            if(status == PipeStatus.CONNECTED)
+            {
+                send(
+                        Packet.OpCode.CLOSE,
+                        new JSONObject(),
+                        null
+                );
+            }
+        }
+        catch(Exception ignored) {}
+
+        status = PipeStatus.CLOSED;
+
+        try
+        {
+            file.close();
+        }
+        catch(Exception ignored) {}
+    }
 }
